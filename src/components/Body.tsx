06a38f37233519d5298b7d292cb2e8b1bef47395
Venukumar Shivagari <vshiva3854@gmail.com>
2024-05-25 18:01:20 +0530

    Text added in Body

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,6 +5,7 @@ import { Outlet } from "react-router-dom";
 const Body = () => {
   return (
     <div className="h-full bg-gradient-to-b from-[#131039] via-[#4db89a] to-[#71a899]">
+      <p>Body</p>
       <Header />
       <Outlet />
     </div>
