21da8f4fd982c25390832fe2cc626b11980ea432
Venukumar Shivagari <vshiva3854@gmail.com>
2024-05-25 18:25:04 +0530

    Body1 text is added

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -6,6 +6,7 @@ const Body = () => {
   return (
     <div className="h-full bg-gradient-to-b from-[#131039] via-[#4db89a] to-[#71a899]">
       <p>Body</p>
+      <p>Body1</p>
       <Header />
       <Outlet />
     </div>
